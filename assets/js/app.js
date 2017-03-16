9dbdd47c4692abd07293d3a8a8c66e03e13138b8
Nicholas N <nnlaor@gmail.com>
2017-03-15 17:07:42 -0700

    adding $.ajax to app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,39 @@
+// put the weather on the top with one gif
+// any entries will include search plus weather 
 
+// display gifs in organized matter
+
+
+var queryURL = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+var queryURL2 = "http://api.openweathermap.org/data/2.5/weather?q="
+
+
+
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).done(function(response) {
+      console.log(response);
+    });
+
+     $.ajax({
+      url: queryURL2,
+      method: 'GET'
+    }).done(function(response) {
+      console.log(response);
+    });
+
+// var title = "space+jam";
+//     var queryURL = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&r=json";
+
+//     $.ajax({
+//       url: queryURL,
+//       method: "GET"
+//     }).done(function(response) {
+//       console.log(response);
+//       console.log(response.Runtime);
+//       console.log(response.Actors)
+//     });
 
 
 
