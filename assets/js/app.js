75d48639bf5a9becef2c12d250eb6b1a006052eb
Nicholas N <nnlaor@gmail.com>
2017-03-17 22:29:44 -0700

    adding button loop addition

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,76 @@
-// put the weather on the top with one gif
-// any entries will include search plus weather 
+var gifMachine = {
+    topics: ['architecture',
+        'parametric',
+        'boids',
+        'lsd',
+        'star fox',
+        'eon flux',
+        'generative',
+        'p5js',
+        'processing',
+        'fluid simpulation',
+        '3d graphics',
+        'loading',
+        'adventure time'
+    ],
 
-// display gifs in organized matter
-
-var query_param = "Anime+Weather";
-var appID = "dc6zaTOxFJmzC"; // public
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + query_param + "&api_key=" + appID;
-console.log(queryURL);
-var query_param1 = "Santa Monica";
-var appID2 = "ad2a32bbb65b8db270ee6f7c72514b20";
-var queryURL2 = "http://api.openweathermap.org/data/2.5/weather?q=" + query_param1 + "&APPID=" + appID2;
+    weatherJax: function() {
 
 
+        var query_param = "Anime+Weather";
+        var appID = "dc6zaTOxFJmzC"; // public
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + query_param + "&api_key=" + appID;
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).done(function(response) {
+            console.log("GIPHY");
+            console.log(response);
+        });
+    },
 
-    $.ajax({
-      url: queryURL,
-      method: 'GET'
-    }).done(function(response) {
-      console.log("GIPHY");  	
-      console.log(response);
-    });
+    giphyJax: function() {
+        var query_param1 = "Santa Monica";
+        var appID2 = "ad2a32bbb65b8db270ee6f7c72514b20";
+        var queryURL2 = "http://api.openweathermap.org/data/2.5/weather?q=" + query_param1 + "&APPID=" + appID2;
+        console.log(this.topics[3]);
 
-     $.ajax({
-      url: queryURL2,
-      method: 'GET'
-    }).done(function(response) {
-      console.log("Weather");
-      console.log(response);
-    });
+        $.ajax({
+            url: queryURL2,
+            method: 'GET'
+        }).done(function(response) {
+            console.log("Weather");
+            console.log(response);
+        });
 
-// var title = "space+jam";
-//     var queryURL = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&r=json";
 
-//     $.ajax({
-//       url: queryURL,
-//       method: "GET"
-//     }).done(function(response) {
-//       console.log(response);
-//       console.log(response.Runtime);
-//       console.log(response.Actors)
-//     });
+    },
+
+    buttonGenerator: function() {
+        $('#buttons').empty();
+        console.log(this.topics[3] + ' in the generator');
+        for (i = 0; i < this.topics.length; i++) {
+            var bttn = $('<button />', {
+                "class": 'abstract',
+                "data-value": this.topics[i].replace(" ", "+"),
+                text: this.topics[i]
+            });
+            $('#buttons').append(bttn);
+
+        }
+    }
+
+
+};
+
+
+
+gifMachine.giphyJax();
+gifMachine.weatherJax();
+gifMachine.buttonGenerator();
+
+
+
 
 
 
